bb8030183ea222ba3697ef69d566dcae5bc7b79b
Patric Stout <truebrain@openttd.org>
2021-07-10 11:53:15 +0200

    Add: small tweaks so the Game Coordinator understands Master Server data (#5)
    This means it can now list older servers that are registered
    via the Master Server (given the Master Server uses the same redis
    as backend).

--- game_coordinator/application/helpers/server.py
+++ game_coordinator/application/helpers/server.py
@@ -13,10 +13,13 @@ class ConnectAndCloseProtocol(asyncio.Protocol):
 
 
 class ServerExternal:
-    def __init__(self, connection_string, info):
-        self.info = info
-        self.game_type = info["game_type"]
-        self.connection_string = connection_string
+    def __init__(self, server_id, game_type):
+        self.info = {}
+        self.game_type = game_type
+        self.server_id = server_id
+
+        self.connection_string = None
+        self.connection_type = ConnectionType.CONNECTION_TYPE_ISOLATED
 
     async def disconnect(self):
         pass
@@ -24,9 +27,19 @@ class ServerExternal:
     async def update(self, info):
         self.info = info
 
+    async def update_direct_ip(self, ip_type, server):
+        # Always use the IPv4 if possible, and only IPv6 if there is no IPv4.
+        if ip_type == "ipv6":
+            if self.connection_string is None:
+                self.connection_string = f"[{server['ip']}]:{server['port']}"
+        else:
+            self.connection_string = f"{server['ip']}:{server['port']}"
+
+        self.connection_type = ConnectionType.CONNECTION_TYPE_DIRECT
+
 
 class Server:
-    def __init__(self, application, source, game_type, server_port):
+    def __init__(self, application, server_id, game_type, source, server_port):
         self._application = application
         self._source = source
         self._server_port = server_port
@@ -34,20 +47,22 @@ class Server:
 
         self.info = {}
         self.game_type = game_type
+        self.server_id = server_id
 
         self.connection_string = f"{str(self._source.ip)}:{self._server_port}"
+        self.connection_type = ConnectionType.CONNECTION_TYPE_ISOLATED
 
     async def disconnect(self):
-        await self._application.database.server_offline(self.connection_string)
+        await self._application.database.server_offline(self.server_id)
 
         if self._task:
             self._task.cancel()
 
     async def update(self, info):
         self.info = info
-        self.info["game_type"] = self.game_type
+        self.info["game_type"] = self.game_type.value
 
-        await self._application.database.update_info(self.connection_string, self.info)
+        await self._application.database.update_info(self.server_id, self.info)
 
     async def detect_connection(self):
         self._task = asyncio.create_task(self._start_detection())
@@ -70,9 +85,10 @@ class Server:
                 ),
                 1,
             )
-            connection_type = ConnectionType.CONNECTION_TYPE_DIRECT
+            await self._application.database.direct_ip(self.server_id, self._source.ip, self._server_port)
+            self.connection_type = ConnectionType.CONNECTION_TYPE_DIRECT
         except (OSError, ConnectionRefusedError, asyncio.TimeoutError):
-            connection_type = ConnectionType.CONNECTION_TYPE_ISOLATED
+            return
 
-        await self._source.protocol.send_PACKET_COORDINATOR_SERVER_REGISTER_ACK(connection_type=connection_type)
+        await self._source.protocol.send_PACKET_COORDINATOR_SERVER_REGISTER_ACK(connection_type=self.connection_type)
         self._task = None
