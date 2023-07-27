a916c36cf902952a00ea1749b9c94cb6c0197b77
Patric Stout <truebrain@openttd.org>
2023-07-27 15:07:48 +0200

    Fix: [Stun] check every 10s for a stale database connection
    Turn and coordinator already do this implicit. Stun now does it
    explicit.

--- game_coordinator/application/stun.py
+++ game_coordinator/application/stun.py
@@ -1,3 +1,4 @@
+import asyncio
 import logging
 
 log = logging.getLogger(__name__)
@@ -10,10 +11,28 @@ class Application:
         log.info("Starting STUN server ...")
 
     async def startup(self):
-        pass
+        asyncio.create_task(self._guard(self._check_database_connection()))
 
     async def shutdown(self):
         log.info("Shutting down STUN server ...")
+
+    async def _guard(self, coroutine):
+        try:
+            await coroutine
+        except asyncio.CancelledError:
+            raise
+        except Exception:
+            log.exception("System co-routine failed, killing server ..")
+
+            import sys
+
+            sys.exit(1)
+
+    async def _check_database_connection(self):
+        # Check every 10 seconds if the database connection is still there.
+        while True:
+            await self.database.ping()
+            await asyncio.sleep(10)
 
     async def receive_PACKET_STUN_SERCLI_STUN(self, source, protocol_version, token, interface_number):
         await self.database.stun_result(token, interface_number, source.ip, source.port)
